78a03ac5f5b6624bf92c312069d4d65de930ae1e
hknarra <51864119+hknarra@users.noreply.github.com>
2021-06-28 12:24:11 +0530

    commit 1

---
+++ Python_Mongo_cloud.py
@@ -0,0 +1,22 @@
+"""
+working with local MongoDB cloud
+"""
+
+import pymongo
+import urllib.parse
+
+username = urllib.parse.quote_plus('mongokittu')
+password = urllib.parse.quote_plus("kittu@1mongo")
+
+cluster = pymongo.MongoClient("mongodb+srv://{}:{}@cluster0.svyqr.mongodb.net/HK_DB1?retryWrites=true&w=majority")
+
+
+dbname='HK_DB1'
+db = cluster[dbname] # connecting to the database named HK_DB1 present in the mongoDB
+                    # if the database is not present, it'll autoamtically create it.
+
+collection_name='HK_Collection1'
+collection=db[collection_name]  # connectig to the collection
+
+
+collection.insert_one({"_id": 0, "name": 'hk'})
