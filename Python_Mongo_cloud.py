71e4eec2a721b16ef931aa388d5fb0a43094e956
hknarra <51864119+hknarra@users.noreply.github.com>
2022-02-26 17:37:15 +0530

    Update Python_Mongo_cloud.py

--- Python_Mongo_cloud.py
+++ Python_Mongo_cloud.py
@@ -5,18 +5,18 @@ working with local MongoDB cloud
 import pymongo
 import urllib.parse
 
-username = urllib.parse.quote_plus('mongokittu')
-password = urllib.parse.quote_plus("kittu@1mongo")
+username = urllib.parse.quote_plus('*********')
+password = urllib.parse.quote_plus("************")
 
-cluster = pymongo.MongoClient("mongodb+srv://{}:{}@cluster0.svyqr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+cluster = pymongo.MongoClient("*************************")
 
 
-dbname='HK_DB1'
+dbname='MY_DB1'
 db = cluster[dbname] # connecting to the database named HK_DB1 present in the mongoDB
                     # if the database is not present, it'll autoamtically create it.
 
-collection_name='HK_Collection1'
+collection_name='MY_Collection1'
 collection=db[collection_name]  # connectig to the collection
 
 
-collection.insert_one({"_id": 0, "name": 'hk'})
+collection.insert_one({"_id": 0, "name": 'ab'})
